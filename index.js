c6acdf2bd56c0ca0937bced85a0824982adc2f3c
enti <enti@enti.enti>
2022-03-04 11:12:36 +0100

    el codigo falla

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 #!/usr/bin/node
+  
+    let http = require("http");
+	  
+    let mongo_client = require("mongodb").MongoClient;
+		  
+    let url = ("mongodb://localhost/");
 
+	let db;
+				 
+		console.log("iniciando script mongo-http");
+				
+		mongo_client.connect(url, function(error, conn)
+		{
+		console.log("Dentro de MongoDB");
+			  
+			if (error)
+			{
+				console.log("ERROR!!!");
+				return;
+			}
+						   
+		 db = conn.db("tffhd");					 
+	});
+	http.createServer(function(req, res){
+	    res.writeHead(200);
 
-let http = require("http")
+		if(req.url == "/")
+		{
+			fs.readFile("index.html", function (err, data) {
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end(data);
+			});
+			res.end();
+			return
+		}
 
-http.createServer(function(req, res){
-		res.writeHead(200);
+		let col = "";
 
-		let saludo = "Dani putaku";
+		if(req.url == "/characters")
+		{
+			col = "characters";
+		}
+		else if(req.url == "/items") 
+		{
+			col = "items";
+		}
+		else 
+		{
+			return;
+		}
+		let col_data = db.collection(col).find();
 
-		res.end(saludo)
-}).listen(1095);
+		col_data.toArray(function(err,data)
+		{
+			let string = JSON.stringify(data);
+			res.end(string);
+		});
+		}).listen(1095);
